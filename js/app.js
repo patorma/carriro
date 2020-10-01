86a9acf3e36aec48865e5e07debcf6f37f3c7455
patorma <pocontreras@ing.ucsc.cl>
2020-10-01 01:34:17 -0300

    se muestra la informacion de un curso en consola

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const carrito = document.querySelector("#carrito");
 //contenedor donde se van a colocar los productos
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
-const vaciarCarritoBTN = document.querySelector("#vaciar-carrito");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
 
 cargarEventListeners();
@@ -16,6 +16,25 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if (e.target.classList.contains("agregar-carrito")) {
-        console.log(e.target);
+        //Situarse en el padre del elemento e.target.parentElement.parentElement)
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado);
     }
+}
+
+//Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector("img").src,
+        titulo: curso.querySelector("h4").textContent,
+        precio: curso.querySelector(".precio span").textContent,
+        id: curso.querySelector("a").getAttribute("data-id"),
+        cantidad: 1
+    };
+
+    console.log(infoCurso);
 }
