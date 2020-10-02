4b8e4d6a2f8d40f1d5e1fd3a444c9dcdc4319dba
patorma <pocontreras@ing.ucsc.cl>
2020-10-02 01:37:42 -0300

    Mostrar en carrito producto seleccionado

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = []
 
 cargarEventListeners();
 //funcion para cargar addEventlistener
 function cargarEventListeners() {
-    //Cuando agregas un curso presionando 'Agregar al Carrito'
+    // 1° Cuando agregas un curso presionando 'Agregar al Carrito'
     listaCursos.addEventListener("click", agregarCurso);
 }
 
 //Funciones
+//cuando se crea un curso se ejecuta esta funcion
 function agregarCurso(e) {
     e.preventDefault();
+
+    //2° Nos aseguramos que el usuario haya hecho click en agregar carrito 
     if (e.target.classList.contains("agregar-carrito")) {
+        // 3°  Y accedemos a todo el div que tiene el contenedor del curso
         //Situarse en el padre del elemento e.target.parentElement.parentElement)
         const cursoSeleccionado = e.target.parentElement.parentElement;
 
@@ -23,9 +28,10 @@ function agregarCurso(e) {
     }
 }
 
+// 4° leemos datos del curso 
 //Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
 function leerDatosCurso(curso) {
-    console.log(curso);
+    // console.log(curso);
 
     //Crear un objeto con el contenido del curso actual
     const infoCurso = {
@@ -35,6 +41,47 @@ function leerDatosCurso(curso) {
         id: curso.querySelector("a").getAttribute("data-id"),
         cantidad: 1
     };
+    // 5° agregamos el curso al carrito
+    // Agrega elementos al arreglo de carrito
+    // se toma una copia del carrito de compra(articulosCarrito)
+    //y se agrega la informacion a este array
+    //no queremos perder la referencia de los cursos agregados
+    //por eso se hace una copia del array
+    //equivalente hacer un push
+    articulosCarrito = [...articulosCarrito, infoCurso]
+    console.log(articulosCarrito)
+        //6 °imprimimos ese HTML
+    carritoHTML()
 
-    console.log(infoCurso);
+}
+
+// Muestra el Carrito de compras en el HTML
+// se encarga de generar el HTML basado en el carrito de compras
+function carritoHTML() {
+
+    // Limpiar el HTML 
+    limpiarHTML()
+
+    // Recoore el carrito y genera el HTML
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('tr')
+        row.innerHTML = `
+          <td>
+              ${curso.titulo}     
+          </td>
+        `
+            // Agrega el HTML del carrito en el body
+        contenedorCarrito.appendChild(row)
+    })
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+    // Forma lenta
+    // contenedorCarrito.innerHTML = ''
+
+    // si ese contenedor carrito tiene aunque sea un elemento dentro (hijo)
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
